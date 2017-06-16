796cd99c8225f2f80166af803ceb4183001f6d0d
Tom Wilkason <tom.wilkason@gmail.com>
2017-06-16 11:13:32 -0400

    Removed unncessary files, pass 1

--- pushx86 (run as admin).bat
+++
@@ -1,6 +0,0 @@
-c:
-cd "%USERPROFILE%\Google Drive\Code\rdt"
-xcopy rdt.exe "C:\Program Files (x86)\Recent Document Tracker (RDT)\" /d /y
-xcopy twapi_base.dll "C:\Program Files (x86)\Recent Document Tracker (RDT)\" /d /y
-xcopy shellicon0.2.dll "C:\Program Files (x86)\Recent Document Tracker (RDT)\" /d /y
-pause
